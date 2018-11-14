94eb5fbed1c0487fcbef3e03c14b5620498fc71a
Juan E. Sanchez <juan@tcad.com>
2018-11-13 21:08:03 -0600

    another attempt

--- scripts/setup_appveyor.sh
+++ scripts/setup_appveyor.sh
@@ -8,14 +8,14 @@ SYMDIFF_CONFIG="appveyor"
 if [ "$1" = x86 ]; then
 GENERATOR="Visual Studio 15 2017"
 BUILDDIR="win32"
-python2base='c:\Miniconda'
-python3base='c:\Miniconda36'
+python2base="c:\\Miniconda"
+python3base="c:\\Miniconda36"
 fi
 if [ "$1" = x64 ]; then
 GENERATOR="Visual Studio 15 2017 Win64"
 BUILDDIR="win64"
-python2base='c:\Miniconda-x64'
-python3base='c:\Miniconda36-x64'
+python2base="c:\\Miniconda-x64"
+python3base="c:\\Miniconda36-x64"
 fi
 
 #/usr/bin/mkdir -p win32
@@ -27,15 +27,15 @@ fi
 #(cd win64; "$CMAKE" -G "Visual Studio 14 Win64" -DSYMDIFF_CONFIG=${SYMDIFF_CONFIG} -DTCLMAIN=ON ..)
 
 libpath=`/usr/bin/cygpath -w $PWD/lib`
-python2bin='${python2base}\python'
-python3bin='${python3base}\python'
+python2bin="${python2base}\\python"
+python3bin="${python3base}\\python"
 
 #echo $libpath
 # TODO: fix to use conda activate
 /usr/bin/mkdir -p bin
 /usr/bin/cat << EOF > bin/symdiff.bat
 @setlocal
-@echo off
+@echo on
 SET PATH=${python2base};${python2base}\\Library\\bin;%PATH%
 SET PYTHONPATH=$libpath
 $python2bin %*
@@ -44,7 +44,7 @@ EOF
 
 /usr/bin/cat << EOF > bin/symdiff_py3.bat
 @setlocal
-@echo off
+@echo on
 SET PATH=${python3base};${python3base}\\Library\\bin;%PATH%
 SET PYTHONIOENCODING=utf-8
 SET PYTHONPATH=$libpath
